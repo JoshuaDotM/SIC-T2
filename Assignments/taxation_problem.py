3d87823a8d645f40c6b4993a742550ddc550ae02
JoshuaDotM <joshua.maries.colet@gmail.com>
2025-06-26 11:36:23 +0530

    made some changes for better readability

--- Assignments/taxation_problem.py
+++ Assignments/taxation_problem.py
@@ -19,12 +19,50 @@ print(f"Name : {name}, Employee ID : {emp_id}")
 print(f" Annual gross salary : {gross_annual_salary} , Standard deduction : 50000 ,Taxable income : {taxable_income}")
 
 print('-----Level-3-----')
-
-if taxable_income>0 and taxable_income <= 300000:
+tax=0
+if taxable_income > 0 and taxable_income <= 300000:
     pass
 elif taxable_income <= 600000:
-    taxable_income=taxable_income-(5/100*taxable_income)
+    tax=taxable_income-(5/100*taxable_income)
 elif taxable_income <= 900000:
-    taxable_income=taxable_income-(10/100*taxable_income)
+    tax=taxable_income-(10/100*taxable_income)
+elif taxable_income <= 1200000:
+    tax=taxable_income-(15/100*taxable_income)
+elif taxable_income < 1500000:
+    tax=taxable_income-(20/100*taxable_income)
+else:
+    tax=taxable_income-(30/100*taxable_income)
+
+#-----Section 87A Rebate-----
+if taxable_income <= 700000:
+    tax=0
+
+h_e_tax=(4/100*taxable_income)
+final_tax=tax + h_e_tax
+print("The final taxation after adding health and Education cess is :",final_tax)
+
+print('-----Level-4-----')
+
+
+net_salary=gross_annual_salary-final_tax
+print(f"Annual gross salary : {gross_annual_salary} Toatal tax payable : {final_tax}, Annual Net salary : {net_salary}")
+
+
+
+print('-----Level-5(Report generation)-----')
+
+
+
+print("Employee Details")
+print("Name : ",name)
+print("Employee ID :",emp_id)
+print("Gross annual salary : ",gross_annual_salary)
+print("Taxable income : ",taxable_income)
+print("Tax payable : ")
+print("Standard deduction : Rupees 50000")
+print("tax to be paid after the New Regime (2023) : ",tax)
+print("Health and Education Cess : ",h_e_tax)
+print("total tax to be paid : ",final_tax)
+print("Annual net salary : ",net_salary)
 
 #Still working on it to make it more readable
