d89e7d7b3f72b112736cb065f071abeee8698fad
JoshuaDotM <joshua.maries.colet@gmail.com>
2025-06-30 09:10:38 +0530

    Assignment added

---
+++ Assignments/recursion_fibonacci.py
@@ -0,0 +1,10 @@
+# Here the initial values are taken as 1 and 2 instead of 0 and 1.
+def fibonacci(n):
+    if n==0:
+        return 1
+    if n==1:
+        return 2
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+
+print(fibonacci(6))
